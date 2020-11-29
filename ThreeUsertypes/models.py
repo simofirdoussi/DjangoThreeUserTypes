17e8bbd5f5a3e15b90b9bf9d3c71dc1fd4693936
simow <you@example.com>
2020-11-29 16:56:52 +0100

    flag creation

--- ThreeUsertypes/models.py
+++ ThreeUsertypes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    is_type1 = models.BooleanField('type1', default=False)
+    is_type2 = models.BooleanField('type2', default=False)
