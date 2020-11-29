4742a66d4a6fbcb464da4b241bfcfa38590f7cde
simow <you@example.com>
2020-11-29 15:53:52 +0100

    login and register templates, views

--- ThreeUsertypes/views.py
+++ ThreeUsertypes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    return render(request, 'ThreeUsertypes/register.html')
+
+def login(request):
+    return render(request, 'ThreeUsertypes/login.html')
